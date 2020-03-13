01e3451a4b72fffea3f39fe54a81c36793f57c1e
Dmitry2489 <iva.klicunov2489@mail.ru>
2020-03-14 01:45:34 +0300

    media

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,19 @@ import "./import/modules";
 (function() {
   // Elements UI
   const header = document.querySelector(".header");
+  const inputName = document.querySelector(".input-name");
+  const inputTel = document.querySelector(".input-tel");
+  const inputCommit = document.querySelector(".input-commit");
+  const labelName = document.querySelector(".label-name");
+  const labelTel = document.querySelector(".label-tel");
+  const labelCommit = document.querySelector(".label-commit");
   // Events
   header.addEventListener("mousemove", parallax);
+  inputName.addEventListener("click", onInputNameClick);
+  inputTel.addEventListener("click", onInputTelClick);
+  inputCommit.addEventListener("click", onInputCommitClick);
 
+  // Parallax header
   function parallax(event) {
     const parallaxSelector = document.querySelector(
       ".header__parallax-background"
@@ -17,5 +27,15 @@ import "./import/modules";
       event.clientY / 20 +
       "px" +
       ")";
+  }
+  // Form contact label animation
+  function onInputNameClick() {
+    labelName.classList.add("lable-top");
+  }
+  function onInputTelClick() {
+    labelTel.classList.add("lable-top");
+  }
+  function onInputCommitClick() {
+    labelCommit.classList.add("lable-top");
   }
 })();
